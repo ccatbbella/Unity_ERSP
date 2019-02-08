2eee0e063aa5e480449654165c8487f7def29462
Yixiao (Bella) Yue <32970034+ccatbbella@users.noreply.github.com>
2019-02-08 08:53:01 -0800

    first commit

---
+++ Assets/Scripts/Size.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Size : MonoBehaviour
+{
+    private float size;
+    private int index;
+    [SerializeField] private float growthTime = 1;
+    [SerializeField] private string fileName;
+    private float[] sizes;
+    private List<Transform> trees = new List<Transform>(0);
+   
+
+    private void Start()
+    {
+        string[] sSizes = GameFunctions.ReadArray(fileName);
+        sizes = new float[sSizes.Length];
+        
+        for(int i = 0; i < sSizes.Length; i++)
+        {
+            sizes[i] = float.Parse(sSizes[i]);
+        }
+
+        index = 0;
+        StartCoroutine(WaitThenChangeSize(0));
+    }
+
+    IEnumerator WaitThenChangeSize(float time)
+    {
+        yield return new WaitForSeconds(time);
+        foreach (Transform t in trees)
+        {
+            t.localScale = Vector3.one * sizes[index];
+        }
+        
+        index++;
+        if (index<sizes.Length)
+        {
+            StartCoroutine(WaitThenChangeSize(growthTime));
+        }
+
+    }
+    public void AddTree(Transform t)
+    {
+        trees.Add(t);
+    }
+}
---
+++ Assets/Scripts/TreeGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//generate 10 resprouters and seeders at random location of terrain
+public class TreeGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject resprouterPrefab;
+    [SerializeField] private GameObject seederPrefab;
+    [SerializeField] private Size rs;
+    [SerializeField] private Size ss;
+    [SerializeField] private int numberOfResprouters = 15;
+    [SerializeField] private int numberOfSeeder = 15;
+    /*
+    public void AddTree(Transform newTree)
+    {
+        newTree.localScale = Vector3.one * 5f;  
+    }
+    */
+        // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject thisTree;
+        for (int i = 0; i< numberOfResprouters;i++)
+        {
+            thisTree = GenerateTree(resprouterPrefab);
+            rs.AddTree(thisTree.transform);
+        }
+        for (int i = 0; i < numberOfSeeder; i++)
+        {
+            thisTree = GenerateTree(seederPrefab);
+            ss.AddTree(thisTree.transform);
+        }
+    }
+    private GameObject GenerateTree(GameObject treePrefab)
+    {
+        Vector3 p = new Vector3(Random.Range(100f, 1000f), 0, Random.Range(100f, 1000f));  //p decide the area trees are in
+        return GameObject.Instantiate(treePrefab, p, Quaternion.identity);
+    }
+    
+}
