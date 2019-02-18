690d8fe09caed83eb0bcb095ac6913d1b19e5c98
Yixiao (Bella) Yue <32970034+ccatbbella@users.noreply.github.com>
2019-02-17 22:43:41 -0800

    finished color.cs

--- Assets/Scripts/color.cs
+++ Assets/Scripts/color.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 
 public class color : MonoBehaviour
 {
-    public Texture2D myTexture;
-    public Color[] myColors;
+    [SerializeField] private GameObject object1;
+    [SerializeField] private GameObject object2;
+    [SerializeField] private int numberOfObject1;
+    [SerializeField] private int numberOfObject2;
+    [SerializeField] private Texture2D myTexture;
+    private Color[] myColors;
+    private Terrain myTerrain;
+    private TerrainData myTerrainData;
+    private List<Vector3> takenCoordinates = new List<Vector3>();
 
     public bool IsSameColor(Color c1, Color c2)
-    {   // if a color's h(hue) value has the same number in the position to the right of decimal point
-        // ex: color 1's hue:0.2, color2's hue:0.27. Function return true
-        // color3's hue : 0.3, color 4's hue: 0.5. Function return false
-        float H1, S1, V1;
-        float H2, S2, V2;
-        Color.RGBToHSV(c1, out H1, out S1, out V1);
-        Color.RGBToHSV(c2, out H2, out S2, out V2);
-      
-        if (Math.Floor(H1*10) != Math.Floor(H2*10))
+    {   // check if c1 and c2 has similar color
+        //basicColorHueValue: [0°:Red, 30°:Orange, 60°:Yellow, 90°:Chartreuse green, 120°:Green, 150°:Spring green, 180°:Cyan, 210°:Azure, 240°:Blue, 270°:Violet, 300°:Magenta, 330°:Rose]
+        //Color.RGBTOHSV return a number from 0-1. It is the hue value(a degree) of the color divides by 360 degree
+        
+        Color.RGBToHSV(c1, out float H1, out float S1, out float V1);
+        Color.RGBToHSV(c2, out float H2, out float S2, out float V2);
+
+        float colorStep = 30f / 360f; //from the List basicColorHueValue above, you can see that every 30 degree, it shifts to another major color
+        if (Math.Floor(H1 / colorStep) != Math.Floor(H2 / colorStep))
         {
             return false;
         }
@@ -25,7 +32,7 @@ public class color : MonoBehaviour
     }
     public bool Find(Color c, List<Color> colors)
     {
-        // Find it Color s is in the colors list
+        // Find if Color s is in the colors list
         foreach(Color color in colors)
         {
             if(IsSameColor(c, color))
@@ -49,29 +56,58 @@ public class color : MonoBehaviour
         return result;
     }
 
-    //public bool isCorrectType() { }
-    public void LocateObject(GameObject ballPrefab, Color c, )
+    public void AllocateObject(GameObject prefab, Color c)
     {
+        //x, z are the coordinates of the top left corner of myTerrain
+        float x = myTerrain.GetPosition().x;
+        float z = myTerrain.GetPosition().z;
+        float width = myTerrainData.size.x; //width of myTerrain
+        float length = myTerrainData.size.z;  //length of myTerrain
 
+        for (int i = 0; i < width*length; i++)
+        {
+            float sampleX = UnityEngine.Random.Range(x, x + width); //x coordinates of my random sample point
+            float sampleZ = UnityEngine.Random.Range(z, z + length);
+            float sampleY = myTerrainData.GetHeight((int)(sampleX), (int)(sampleZ));
+            Color samplePointColor = myTexture.GetPixel((int)(sampleX), (int)(sampleZ));
+            Vector3 samplePoint = new Vector3(sampleX, sampleY, sampleZ);
+
+            if (IsSameColor(c, samplePointColor) && takenCoordinates.IndexOf(samplePoint) == -1)
+            {
+                GameObject.Instantiate(prefab, samplePoint, Quaternion.identity);
+                takenCoordinates.Add(samplePoint);
+                return;
+            }
+        }
+    }
+
+    public void AllocateObjects(GameObject prefab, Color c, int numberOfObjects)
+    {
+        for(int i = 0; i < numberOfObjects; i++)
+        {
+            AllocateObject(prefab, c);
+        }
     }
     
     void Start()
     {
-        float H1, S1, V1;
-    
+        myTerrain = Terrain.activeTerrain;
+        myTerrainData = Terrain.activeTerrain.terrainData;
+
         //GetPixels get an Color[] of the Color of all Pixels in the Texture.
         //myTexture is a public property. When run, assign your png to myTexture by using the inspector
         myColors = myTexture.GetPixels();
         List<Color> result = GetColorList(myColors);
         
-        //THis loop is for debugging, to print the hue of the list of color I have from GetColorList
-        //If the number printed all have different number to the right of the decimal, and the number is not huge, it is working
+        //This loop is for debugging, to print the hue of the list of color I have from GetColorList
         foreach(Color c in result)
         {
-            Color.RGBToHSV(c, out H1, out S1, out V1);
+            Color.RGBToHSV(c, out float H1, out float S1, out float V1);
             Debug.Log(H1);
         }
 
-       
+        Color.RGBToHSV(result[2], out float H2, out float S2, out float V2);
+        AllocateObjects(object1, result[0], numberOfObject1);
+        AllocateObjects(object2, result[1], numberOfObject2);
     }
 }
